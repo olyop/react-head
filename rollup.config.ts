8b969d82a8c16f2b135aa8605fd5c6968de66eef
olyop <oliver.plummer@outlook.com>
2022-01-10 19:53:46 +1100

    Update

---
+++ rollup.config.ts
@@ -0,0 +1,27 @@
+import jsx from "acorn-jsx"
+import { RollupOptions } from "rollup"
+import typescript from "@rollup/plugin-typescript"
+
+const options: RollupOptions = {
+	input: "src/index.tsx",
+	output: [{
+		format: "cjs",
+		strict: false,
+		exports: "named",
+		file: "build/index.js",
+	}],
+	acornInjectPlugins: [
+		jsx(),
+	],
+	plugins: [
+		typescript({
+			tsconfig: "./tsconfig.rollup.json",
+		}),
+	],
+	external: [
+		"react",
+		"react-dom",
+	],
+}
+
+export default options
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import {
 	FC,
+	useMemo,
 	Fragment,
 	useEffect,
 	useContext,
@@ -8,45 +9,93 @@ import {
 } from "react"
 
 const setMetaTag =
-	(type: string, text: string) =>
-		document
-			.querySelector<HTMLElement>(`meta[name="${type}"]`)!
-			.setAttribute("content", text)
-
-const setMetadata =
-	(appTitle: string, title: string) => {
-		const text = `${title} - ${appTitle}`
-		document.title = text
-		setMetaTag("keywords", text)
-		setMetaTag("og:title", text)
-		setMetaTag("description", text)
-		setMetaTag("og:description", text)
+	(type: string, text: Parameters<Element["setAttribute"]>[1]) => {
+		const element =
+			document.querySelector<HTMLElement>(`meta[name="${type}"]`)
+		if (element) {
+			element.setAttribute("content", text)
+		}
 	}
 
-interface Context {
+interface AppTitle {
 	appTitle: string,
 }
 
-const MetadataContext =
-	createContext<Context>({ appTitle: "" })
-
-export const MetadataProvider =
-	MetadataContext.Provider
-
-export const Metadata: FC<MetadataPropTypes> = ({ title, children }) => {
-	const { appTitle } = useContext(MetadataContext)
-
-	useEffect(() => {
-		if (title) {
-			setMetadata(appTitle, title)
-		}
-	}, [title])
-	return (
-		<Fragment>
-			{children}
-		</Fragment>
-	)
+interface PageTitle {
+	pageTitle: string,
 }
+
+interface MetadataOptions
+	extends AppTitle, PageTitle {}
+
+export type ParseTitleFunction =
+	(parseOptions: MetadataOptions) => string
+
+interface ParseTitleOptions {
+	parseTitle: ParseTitleFunction,
+}
+
+interface SetMetadataOptions
+	extends ParseTitleOptions {
+	metadata: MetadataOptions,
+}
+
+export interface Context
+	extends ParseTitleOptions, AppTitle {}
+
+const defaultParseTitleFunction: ParseTitleFunction =
+	({ appTitle, pageTitle }) =>
+		`${pageTitle} - ${appTitle}`
+
+const setMetadata =
+	({ parseTitle, metadata }: SetMetadataOptions) => {
+		const title = parseTitle(metadata)
+		document.title = title
+		setMetaTag("keywords", title)
+		setMetaTag("og:title", title)
+		setMetaTag("description", title)
+		setMetaTag("og:description", title)
+	}
+
+const MetadataContext =
+	createContext<Context>({ appTitle: "", parseTitle: defaultParseTitleFunction })
+
+export const MetadataProvider: FC<MetadataProviderPropTypes> =
+	({ children, appTitle, parseTitle = defaultParseTitleFunction }) => {
+		const value = useMemo<Context>(() => ({ appTitle, parseTitle }), [])
+		return (
+			<MetadataContext.Provider value={value}>
+				{children}
+			</MetadataContext.Provider>
+		)
+	}
+
+export interface MetadataProviderPropTypes
+	extends AppTitle {
+	parseTitle?: ParseTitleFunction,
+}
+
+export const Metadata: FC<MetadataPropTypes> =
+	({ title, children }) => {
+		const { appTitle, parseTitle } =
+			useContext(MetadataContext)
+
+		useEffect(() => {
+			setMetadata({
+				parseTitle,
+				metadata: {
+					appTitle,
+					pageTitle: title,
+				},
+			})
+		}, [title])
+
+		return (
+			<Fragment>
+				{children}
+			</Fragment>
+		)
+	}
 
 export interface MetadataPropTypes {
 	title: string,
